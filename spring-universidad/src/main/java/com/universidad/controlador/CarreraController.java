46c69a60379e93289e3e7e46910b06eaebd0a028
Cristian Revelo <cristianrevelo98@gmail.com>
2022-09-07 11:31:40 -0600

    Se agrego peticion al controller getID

--- spring-universidad/src/main/java/com/universidad/controlador/CarreraController.java
+++ spring-universidad/src/main/java/com/universidad/controlador/CarreraController.java
@@ -5,10 +5,12 @@ import com.universidad.modelo.entidades.Carrera;
 import com.universidad.servicios.contratos.CarreraDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/carreras")
@@ -27,5 +29,14 @@ public class CarreraController {
             throw new BadRequestException("No existen carreras");
         }
         return carreras;
+    }
+
+    @RequestMapping(value = "/{codigo}")
+    public Carrera obtenerPorId(@PathVariable(value = "codigo",required = false)Integer id){
+        Optional<Carrera> oCarrera = carreraDAO.findById(id);
+        if(!oCarrera.isPresent()){
+            throw new BadRequestException(String.format("La carrera con id %d no existe",id));
+        }
+        return oCarrera.get();
     }
 }
